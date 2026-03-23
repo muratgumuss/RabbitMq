d41c87d58b043e23b8ee15c683e6ab2aa97423db
Murat Gümüş <muratgumus05@gmail.com>
2026-03-23 17:08:05 +0300

    Direct Exchange

--- RabbitMq.Publisher/Program.cs
+++ RabbitMq.Publisher/Program.cs
@@ -1,8 +1,16 @@
 ﻿using RabbitMQ.Client;
 using System.Text;
+using static Program;
 
 public class Program
 {
+    public enum LogLevel
+    {
+        Info = 1,
+        Warning = 2,
+        Error = 3,
+        Critical = 4
+    }
     private static async Task Main(string[] args)
     {
         var factory = new ConnectionFactory();
@@ -10,21 +18,39 @@ public class Program
 
         using var connection = await factory.CreateConnectionAsync();
         using var channel = await connection.CreateChannelAsync();
-        await channel.QueueDeclareAsync(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+        //await channel.QueueDeclareAsync(queue: "hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-        Enumerable.Range(1,50).ToList().ForEach(i =>
+        //await channel.ExchangeDeclareAsync("logs-fanout", durable: true, type: ExchangeType.Fanout);
+        await channel.ExchangeDeclareAsync("logs-direct", durable: true, type: ExchangeType.Direct);
+
+        foreach (var logLevel in Enum.GetNames(typeof(LogLevel)))
         {
-            var message = $"Message {i}";
+            var routeKey = $"route-{logLevel}"; // ✅ tutarlı: hep lowercase
+            var queueName = $"direct-queue-{logLevel}";
+            await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            await channel.QueueBindAsync(queue: queueName, exchange: "logs-direct", routingKey: routeKey);
+        }
+
+        foreach (var i in Enumerable.Range(1, 50))
+        {
+            LogLevel loglevel = (LogLevel)new Random().Next(1, 5);
+
+            var message = $"log-type: {loglevel}";
             var messageBody = Encoding.UTF8.GetBytes(message);
             var properties = new BasicProperties();
-            channel.BasicPublishAsync(
-                        exchange: string.Empty,
-                        routingKey: "hello-queue",
-                        mandatory: false,
-                        basicProperties: properties,
-                        body: messageBody);
-            Console.WriteLine($"Mesaj {i} gönderildi! {message}");
-        });
+
+            var routeKey = $"route-{loglevel.ToString()}"; // ✅ tutarlı: hep lowercase
+
+            await channel.BasicPublishAsync(
+                exchange: "logs-direct",
+                routingKey: routeKey,
+                mandatory: false,
+                basicProperties: properties,
+                body: messageBody);
+
+            Console.WriteLine($"Log {i} gönderildi! {message}");
+        }
+
 
         //var message = "Hello World!";
         //var messageBody = Encoding.UTF8.GetBytes(message);
