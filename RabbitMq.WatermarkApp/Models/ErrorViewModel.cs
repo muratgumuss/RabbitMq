3a6a0287a82764c54b6fb5c5e94fbf5b514f35b3
Murat Gümüş <muratgumus05@gmail.com>
2026-03-24 20:08:31 +0300

    Rabbitmq watermark app

---
+++ RabbitMq.WatermarkApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RabbitMq.WatermarkApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
