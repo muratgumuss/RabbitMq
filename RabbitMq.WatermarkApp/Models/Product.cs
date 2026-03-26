54cdf079ebde208af482f10cc2c2f3975d752903
Murat Gümüş <muratgumus05@gmail.com>
2026-03-26 16:20:38 +0300

    watermark app completed

--- RabbitMq.WatermarkApp/Models/Product.cs
+++ RabbitMq.WatermarkApp/Models/Product.cs
@@ -8,12 +8,12 @@ namespace RabbitMq.WatermarkApp.Models
         [Key]
         public int Id { get; set; }
         [StringLength(100)]
-        public string Name { get; set; }
+        public string? Name { get; set; }
         [Column(TypeName = "decimal(18,2")]
         public decimal Price { get; set; }
         [Range(1, 100)]
         public int Stock { get; set; }
         [StringLength(100)]
-        public string ImageName { get; set; }
+        public string? ImageName { get; set; } = null;
     }
 }
---
+++ RabbitMq.WatermarkApp/Services/RabbitMQClientService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using RabbitMQ.Client;
+
+namespace RabbitMq.WatermarkApp.Services
+{
+    public class RabbitMQClientService : IDisposable
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IChannel _channel;
+        public static string ExchangeName = "ImageDirectExchange";
+        public static string RoutingWatermark = "watermark-route-image";
+        public static string QueueName = "queue-watermark-image";
+
+        private readonly ILogger<RabbitMQClientService> _logger;
+
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+
+        }
+
+        public async Task<IChannel> Connect()
+        {
+            _connection = await _connectionFactory.CreateConnectionAsync();
+
+
+            if (_channel is { IsOpen: true })
+            {
+                return _channel;
+            }
+
+            _channel = await _connection.CreateChannelAsync();
+
+            await _channel.ExchangeDeclareAsync(ExchangeName, type: "direct", true, false);
+
+            await _channel.QueueDeclareAsync(QueueName, true, false, false, null);
+
+
+            await _channel.QueueBindAsync(exchange: ExchangeName, queue: QueueName, routingKey: RoutingWatermark);
+
+            _logger.LogInformation("RabbitMQ ile bağlantı kuruldu...");
+
+
+            return _channel;
+
+        }
+
+        public async void Dispose()
+        {
+            await _channel?.CloseAsync();
+            _channel?.DisposeAsync().GetAwaiter();
+
+            await _connection?.CloseAsync();
+            _connection?.DisposeAsync().GetAwaiter();
+
+            _logger.LogInformation("RabbitMQ ile bağlantı koptu...");
+
+        }
+    }
+}
