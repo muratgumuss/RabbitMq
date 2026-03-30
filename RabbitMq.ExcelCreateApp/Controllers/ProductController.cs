d4dcf505de9cf98adb79e0af4fca38137215588c
Murat Gümüş <muratgumus05@gmail.com>
2026-03-30 19:28:58 +0300

    RabbitMq ExcelCreateApp With EF Identity

---
+++ RabbitMq.ExcelCreateApp/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMq.ExcelCreateApp.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
